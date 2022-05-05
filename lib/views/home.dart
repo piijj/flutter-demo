a6dafeb2eddfe216eabb9bafbd4c7d0e7f03f15b
cholo recto <pocholorecto123@gmail.com>
2022-05-05 14:45:43 +0800

    feat: added footer (#1)
    * added footer
    
    * active tab color
    
    * updated icons

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,7 +1,9 @@
 import 'dart:developer';
+
 import 'package:field_flutter/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 /// Displayed as a profile image if the user doesn't have one.
 const placeholderImage =
@@ -26,6 +28,8 @@ class HomeState extends State<Home> {
   bool showSaveButton = false;
   bool isLoading = false;
 
+  int _selectedIndex = 0;
+
   @override
   void initState() {
     user = FirebaseAuth.instance.currentUser!;
@@ -43,45 +47,79 @@ class HomeState extends State<Home> {
     super.initState();
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: FocusScope.of(context).unfocus,
       child: Scaffold(
-        body: Stack(
-          children: [
-            Center(
-              child: SizedBox(
-                width: 400,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Stack(
-                      children: [
-                        CircleAvatar(
-                          maxRadius: 60,
-                          backgroundImage: NetworkImage(
-                            user.photoURL ?? placeholderImage,
+          body: Stack(
+            children: [
+              Center(
+                child: SizedBox(
+                  width: 400,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                            maxRadius: 60,
+                            backgroundImage: NetworkImage(
+                              user.photoURL ?? placeholderImage,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Text(user.displayName ?? ''),
-                    Text(user.email ?? user.phoneNumber ?? 'User'),
-                    const SizedBox(height: 10),
-                    const SizedBox(height: 40),
-                    TextButton(
-                      onPressed: () => AuthService().signOut(),
-                      child: const Text('Sign out'),
-                    ),
-                  ],
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Text(user.displayName ?? ''),
+                      Text(user.email ?? user.phoneNumber ?? 'User'),
+                      const SizedBox(height: 10),
+                      const SizedBox(height: 40),
+                      TextButton(
+                        onPressed: () => AuthService().signOut(),
+                        child: const Text('Sign out'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.house),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.listCheck),
+                  label: 'My Tasks',
+                ),
+                BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.map),
+                  label: 'Maps',
+                ),
+                BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.bell),
+                  label: 'Notifications',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              backgroundColor: Colors.white,
+              selectedItemColor: const Color.fromRGBO(18, 120, 236, 1),
+              unselectedItemColor: const Color.fromRGBO(102, 112, 148, 1),
+              selectedFontSize: 12,
+              unselectedFontSize: 12,
+              showUnselectedLabels: true,
+              iconSize: 14,
+              onTap: _onItemTapped)),
     );
   }
 }
