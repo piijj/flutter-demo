fce12cb098e0f8f0d2c4cd7b986b9f600bb1c92e
Ma. Pauline Joy Pama <pjpama6@gmail.com>
2022-05-12 09:09:11 +0800

    move onPressed to container

--- lib/views/my_tasks_page/task_item.dart
+++ lib/views/my_tasks_page/task_item.dart
@@ -19,83 +19,87 @@ class TaskItem extends HookWidget {
     final dispatch = useDispatch<AppState>();
     return SizedBox(
         height: 60,
-        child: Container(
-          decoration: const BoxDecoration(
-              border: Border(
-                  bottom:
-                      BorderSide(width: 1.0, color: AppColors.borderLight))),
-          child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        taskName,
-                        style: const TextStyle(
-                            color: AppColors.title,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Visibility(
-                          visible: parentName.isNotEmpty,
-                          child: Row(
-                            children: [
-                              const FaIcon(FontAwesomeIcons.codeBranch,
-                                  color: AppColors.primary, size: 10),
-                              const SizedBox(width: 4),
-                              Text(
-                                parentName,
-                                style: const TextStyle(
-                                    color: AppColors.label,
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 12),
-                              ),
-                            ],
-                          )),
-                    ],
-                  ),
-                  Row(
+        child: GestureDetector(
+            onTap: () => dispatch(SwitchMyTaskTab(myTaskTab: 1)),
+            child: Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(
+                          width: 1.0, color: AppColors.borderLight))),
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                          width: 46,
-                          height: 46,
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                                backgroundColor: AppColors.lightGrey,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.circular(12), // <-- Radius
-                                )),
-                            child: SvgPicture.asset('assets/svg/thumbs_up.svg',
-                                width: 18, height: 18),
-                            onPressed: () =>
-                                dispatch(SwitchMyTaskTab(myTaskTab: 1)),
-                          )),
-                      const SizedBox(width: 8),
-                      SizedBox(
-                          width: 46,
-                          height: 46,
-                          child: TextButton(
-                              style: TextButton.styleFrom(
-                                  backgroundColor: AppColors.lightGrey,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius:
-                                        BorderRadius.circular(12), // <-- Radius
-                                  )),
-                              child: SvgPicture.asset(
-                                  'assets/svg/thumbs_down.svg',
-                                  width: 18,
-                                  height: 18),
-                              onPressed: () =>
-                                  dispatch(SwitchMyTaskTab(myTaskTab: 1))))
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            taskName,
+                            style: const TextStyle(
+                                color: AppColors.title,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          Visibility(
+                              visible: parentName.isNotEmpty,
+                              child: Row(
+                                children: [
+                                  const FaIcon(FontAwesomeIcons.codeBranch,
+                                      color: AppColors.primary, size: 10),
+                                  const SizedBox(width: 4),
+                                  Text(
+                                    parentName,
+                                    style: const TextStyle(
+                                        color: AppColors.label,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 12),
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                              width: 46,
+                              height: 46,
+                              child: TextButton(
+                                style: TextButton.styleFrom(
+                                    backgroundColor: AppColors.lightGrey,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                          12), // <-- Radius
+                                    )),
+                                child: SvgPicture.asset(
+                                    'assets/svg/thumbs_up.svg',
+                                    width: 18,
+                                    height: 18),
+                                onPressed: () =>
+                                    debugPrint('thumbs up pressed'),
+                              )),
+                          const SizedBox(width: 8),
+                          SizedBox(
+                              width: 46,
+                              height: 46,
+                              child: TextButton(
+                                  style: TextButton.styleFrom(
+                                      backgroundColor: AppColors.lightGrey,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(
+                                            12), // <-- Radius
+                                      )),
+                                  child: SvgPicture.asset(
+                                      'assets/svg/thumbs_down.svg',
+                                      width: 18,
+                                      height: 18),
+                                  onPressed: () =>
+                                      debugPrint('thumbs down pressed')))
+                        ],
+                      )
                     ],
-                  )
-                ],
-              )),
-        ));
+                  )),
+            )));
   }
 }
